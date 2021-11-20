618b75298a047402a6896a71d58e99e7b52548c0
PYK <you@example.com>
2021-11-20 19:00:30 +0530

    First Commit

---
+++ account/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model  
+from account.models import Account 
+
+class AccountAdmin(UserAdmin):
+	list_display=('username','email','date_joined','last_login','is_admin', 'is_staff')
+	search_fields=('email','username',)
+	readonly_fields=('date_joined','last_login')
+
+	filter_horizontal=()
+	list_filter=()
+	fieldsets=()
+
+
+
+	
+
+
+
+
+
+
+
+
+admin.site.register(Account, AccountAdmin)
+
